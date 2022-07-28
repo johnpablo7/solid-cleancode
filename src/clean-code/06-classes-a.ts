b7b22533122313532603108ae68688772b1f8aa0
john <john.davis.2015.03@gmail.com>
2022-07-27 19:09:31 -0500

    First commit

---
+++ src/clean-code/06-classes-a.ts
@@ -0,0 +1,26 @@
+(() => {
+  // No aplicando el principio de responsabilidad única
+
+  type Gender = "M" | "F";
+  // class Person {
+  //   public name: string;
+  //   public gender: Gender;
+  //   public birthdate: Date;
+
+  //   constructor(name: string, gender: Gender, birthdate: Date) {
+  //     this.name = name;
+  //     this.gender = gender;
+  //     this.birthdate = birthdate;
+  //   }
+  // }
+  class Person {
+    constructor(
+      public name: string,
+      public gender: Gender,
+      public birthdate: Date
+    ) {}
+  }
+
+  const newPerson = new Person("Fernando", "M", new Date("1985-10-21"));
+  console.log({ newPerson });
+})();
